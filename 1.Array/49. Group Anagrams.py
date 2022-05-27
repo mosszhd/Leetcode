bf53f705c817c0737964923da1881a4f2e20b518
mosszhd <mustafakamalmoss@gmail.com>
2022-05-27 19:24:59 +0600

    added new solution

---
+++ 1.Array/49. Group Anagrams.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def groupAnagrams(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: List[List[str]]
+        """
+        anas = {}
+        for string in strs:
+            s = ''.join(sorted(string))
+            if s in anas:
+                anas[s].append(string)
+            else:
+                anas[s] = [string]
+        return [ anas[x] for x in anas ]
+
+sol = Solution()
+print(sol.groupAnagrams(["eat","tea","tan","ate","nat","bat"]))
